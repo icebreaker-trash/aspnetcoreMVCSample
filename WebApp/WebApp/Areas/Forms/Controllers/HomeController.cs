2d7deaa24df0e1b2200e4de5c4a673ad6d3c7957
SonOfMagic <example@same.com>
2017-06-19 00:51:46 +0800

    update form area initialization

--- WebApp/WebApp/Areas/Forms/Controllers/HomeController.cs
+++ WebApp/WebApp/Areas/Forms/Controllers/HomeController.cs
@@ -9,8 +9,52 @@ namespace WebApp.Areas.Forms.Controllers
     [Area("Forms")]
     public class HomeController : Controller
     {
-        public IActionResult GeneralForms()
+        public IActionResult Index()
         {
+            ViewBag.navigation = "General Forms";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult FormLayouts()
+        {
+            ViewBag.navigation = "Form Layouts";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult FormValidation()
+        {
+            ViewBag.navigation = "Form Validation";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult FormWizards()
+        {
+            ViewBag.navigation = "Form Wizards";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult WYSIWYG()
+        {
+            ViewBag.navigation = "WYSIWYG";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult CodeEditor()
+        {
+            ViewBag.navigation = "Code Editor";
+            ViewBag.area = "Forms";
+            return View();
+        }
+
+        public IActionResult X_Editable()
+        {
+            ViewBag.navigation = "X-Editable";
+            ViewBag.area = "Forms";
             return View();
         }
     }
